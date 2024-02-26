311451ace2333441ef01e9c460b80128a9796790
dianprsty <dprasmail@gmail.com>
2024-02-26 12:20:41 +0700

    navbar and hero

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,22 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.scss";
 
-console.log('Hello Coders! :)');
+const hamburger = document.getElementById("hamburger");
+const drawer = document.getElementById("drawer");
+const content = document.getElementById("content");
+const closeDrawer = document.getElementById("close-drawer");
+
+hamburger.addEventListener("click", (event) => {
+  drawer.classList.add("open");
+  event.stopPropagation();
+});
+
+content.addEventListener("click", (event) => {
+  drawer.classList.remove("open");
+  event.stopPropagation();
+});
+
+closeDrawer.addEventListener("click", (event) => {
+  drawer.classList.remove("open");
+  event.stopPropagation();
+});
